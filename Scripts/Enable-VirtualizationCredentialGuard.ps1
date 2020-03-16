96397dd3bf68d4c6385c25a31586710444b28b5d
Erik Arnesen <earnesen@ymail.com>
2020-03-16 06:50:34 -0400

    Lots of edits
    Starting a re-edit of the original 3.6
    to 4.0.  Not breaking things this time.  Changing the outputs first.
    
    4.1 - added "write-" to all of the "log" functions.  So that it is easier to read "write-log"

---
+++ Scripts/Enable-VirtualizationCredentialGuard.ps1
@@ -0,0 +1,76 @@
+﻿<#
+.SYNOPSIS
+    Enable virtualization in Lenovo Bios and enable Credential Guard in Windows 10
+.DESCRIPTION
+    This script will only run on Lenovo computers. If run on Lenovo computer, the script will check if virtualization is enabled in BIOS. 
+    If not, virtualization will be enabled in the process of enabling CredentialGuard.
+    Also appends actions to logfile: EnableCredentialGuard.log
+ 
+.NOTES
+    FileName:    Enable-VirtualizationCredentialGuard.ps1
+    Author:      Martin Bengtsson
+    Created:     19-07-2017
+#>
+
+$Logfile = "C:\Windows\EnableCredentialGuard.log"
+
+#Create LogWrite function
+Function LogWrite
+{
+   Param ([string]$Logstring)
+
+   Add-Content $Logfile -Value $Logstring
+}
+
+#Get computermanufacturer
+$Lenovo = Get-WmiObject Win32_ComputerSystemProduct | Select-Object Vendor
+
+#If not a Lenovo laptop, write to log and exit script
+If ($Lenovo.Vendor -ne "Lenovo"){
+    
+    LogWrite "Not a Lenovo laptop - exiting script"
+    Write-Warning -Message "Not a Lenovo laptop - exiting script" ; exit 1
+}
+
+Else {
+    
+    Write-Host -ForegroundColor Yellow "Collecting Lenovo_BiosSetting information" ; LogWrite "Collecting Lenovo_BiosSetting information"
+    $VirtEnabled = Get-WmiObject -Class Lenovo_BiosSetting -Namespace root\WMI | Where-Object {$_.CurrentSetting -match "Virtualization*"} | Select-Object CurrentSetting
+
+If ($VirtEnabled.CurrentSetting -eq "VirtualizationTechnology,Disable"){
+    
+    Write-Host -ForegroundColor Cyan "Virtualization disabled - trying to enable virtualization" ; LogWrite "Virtualization disabled - trying to enable virtualization"
+    Try {
+        (Get-WmiObject -Class Lenovo_SetBiosSetting -Namespace root\wmi).SetBiosSetting("VirtualizationTechnology,Enable")
+        (Get-WmiObject -Class Lenovo_SaveBiosSettings -Namespace root\wmi).SaveBiosSettings()
+
+    }
+    Catch {
+        Write-Host -ForegroundColor Cyan "An error occured when enabling virtualization in the BIOS" ; LogWrite "An error occured when enabling virtualization in the BIOS" ; exit 1
+    }
+    cls
+    Write-Host -ForegroundColor Cyan "Virtualization Successfully enabled" ; LogWrite "Virtualization Successfully enabled"
+    
+}
+
+#Add required registry key for Credential Guard
+$RegistryKeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\DeviceGuard"
+    If (-not(Test-Path -Path $RegistryKeyPath)) {
+        Write-Host -ForegroundColor Yellow "Creating HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\DeviceGuard registry key" ; LogWrite "Creating HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\DeviceGuard registry key"
+        New-Item -Path $RegistryKeyPath -ItemType Directory -Force
+    }
+    #Add registry key: RequirePlatformSecurityFeatures - 1 for Secure Boot only, 3 for Secure Boot and DMA Protection
+    New-ItemProperty -Path $RegistryKeyPath -Name RequirePlatformSecurityFeatures -PropertyType DWORD -Value 1
+    Write-Host -ForegroundColor Yellow "Successfully added RequirePlatformSecurityFeatures regkey" ; LogWrite "Successfully added RequirePlatformSecurityFeatures regkey"
+    
+    #Add registry key: EnableVirtualizationBasedSecurity - 1 for Enabled, 0 for Disabled
+    New-ItemProperty -Path $RegistryKeyPath -Name EnableVirtualizationBasedSecurity -PropertyType DWORD -Value 1
+    Write-Host -ForegroundColor Yellow "Successfully added EnableVirtualizationBasedSecurity regkey" ; LogWrite "Successfully added EnableVirtualizationBasedSecurity regkey"
+    
+    #Add registry key: LsaCfgFlags - 1 enables Credential Guard with UEFI lock, 2 enables Credential Guard without lock, 0 for Disabled
+    New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name LsaCfgFlags -PropertyType DWORD -Value 2
+    Write-Host -ForegroundColor Yellow "Successfully added LsaCfgFlags regkey" ; LogWrite "Successfully added LsaCfgFlags regkey"
+    
+    Write-Host -ForegroundColor Yellow "Successfully enabled Credential Guard - please reboot the computer" ; LogWrite "Successfully enabled Credential Guard - please reboot the computer"
+    
+}   
---
+++ Scripts/Write-Registry.ps1
@@ -0,0 +1,45 @@
+﻿    function Write-Registry
+    {
+      <#
+          .SYNOPSIS
+          Used to write to the registry
+          .NOTES
+          String: Specifies a null-terminated string. Equivalent to REG_SZ.
+          ExpandString: Specifies a null-terminated string that contains unexpanded references to environment variables that are expanded when the value is retrieved. Equivalent to REG_EXPAND_SZ.
+          Binary: Specifies binary data in any form. Equivalent to REG_BINARY.
+          DWord: Specifies a 32-bit binary number. Equivalent to REG_DWORD.
+          MultiString: Specifies an array of null-terminated strings terminated by two null characters. Equivalent to REG_MULTI_SZ.
+          Qword: Specifies a 64-bit binary number. Equivalent to REG_QWORD.
+          Unknown: Indicates an unsupported registry data type, such as REG_RESOURCE_LIST.
+      #>
+
+      param(
+        [Parameter(Mandatory)][String]$registryPath,
+        [Parameter(Mandatory = $true)][String]$Name,
+        [Parameter(Mandatory = $true)][String]$value,
+        [Parameter(Mandatory = $true,HelpMessage = 'String=REG_SZ: ExpandString=REG_EXPAND_SZ: Binary=REG_BINARY: DWord=REG_DWORD: MultiString=REG_MULTI_SZ: Qword=REG_QWORD: Unknown=REG_RESOURCE_LIST')]
+        [ValidateSet('String','ExpandString','Binary','DWord','MultiString','Qword','Unknown')] 
+        [String]$PropertyType
+      )
+      $FunctionMessage = $MyInvocation.MyCommand
+      Write-Verbose -Message ('Entering function: {0}' -f $FunctionMessage) #-Verbose
+      #Write-Warning -Message ('{0}: {1}' -f $FunctionMessage, $($MessageInfo.Deprecated))
+      
+      IF(!(Test-Path -Path $registryPath))
+
+      {
+        Write-Verbose -Message 'Writing to Registry'
+        $null = New-Item -Path $registryPath -Force
+        $null = New-ItemProperty -Path $registryPath -Name $Name -Value $value -PropertyType $PropertyType -Force
+      }
+
+      ELSE 
+      {
+        $null = New-ItemProperty -Path $registryPath -Name $Name -Value $value -PropertyType $PropertyType -Force
+      }
+    }
+
+    #Write-Registry -registryPath 'HKCU:\SYSTEM\CurrentControlSet\Control\DeviceGuard' -Name 'EnableVirtualizationBasedSecurity' -value 1 -PropertyType DWORD
+   
+
+Write-Registry -registryPath $registryPath -Name 'HVCI_Capable' -value 2 -PropertyType DWord
